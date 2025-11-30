45d2b79d2956385dac79ba39621d24a6917a042d
Đại Ca Tính <macos@192.168.2.76>
2025-11-30 23:06:27 +0700

    class component and state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,17 @@
 // class component
 // function component
 
+import { toContainHTML } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Tinh",
+    adress: "Da Nang",
+    age: 24,
+  };
   render() {
-    return <div> my firt component </div>;
+    return <div>my name is {this.state.name}</div>;
   }
 }
 
