39d8b1f94240926067a03acad67281abdcfa7c0d
Đại Ca Tính <macos@192.168.2.76>
2025-12-07 23:05:37 +0700

    setStae

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,20 +10,25 @@ class MyComponent extends React.Component {
     adress: "Da Nang",
     age: 24,
   };
-  handleClick(event) {
+  handleClick = (event) => {
     console.log(">> click me my button");
-    console.log(event);
-  }
+    console.log(" My name is ", this.state.name);
 
-  handleOnMoverOver(event) {
+    this.setState({
+      name: "Nam", //cập nhật tên mới thay thế cho tên ban đầu
+    });
+  };
+
+  handleOnMoverOver = (event) => {
     console.log(event.pageX);
-  }
+  };
 
   render() {
     return (
       <div>
         my name is {this.state.name}
         <button onClick={this.handleClick}>Click me</button>
+        {/* cach khac <button onClick={(event) => {this.handleClick(event)}}>Click me</button> */}
         <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
       </div>
     );
