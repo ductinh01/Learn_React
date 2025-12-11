34289d2f7ab54246e59c398179de1e429fbe0088
Đại Ca Tính <macos@192.168.2.76>
2025-12-11 20:59:16 +0700

    destructuring

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,10 +2,12 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
   render() {
+    //destructuring array/object
+    const { age, name } = this.props; //object
     return (
       <div>
-        <div>My name's {this.props.name}</div>
-        <div>My name's {this.props.age}</div>
+        <div>My name's {name}</div>
+        <div>My name's {age}</div>
       </div>
     );
   }
